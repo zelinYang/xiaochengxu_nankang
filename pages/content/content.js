7dfb7feb8cdcaa495c91295887364fb1cdb1e2c0
YZL <YZL@qq.com>
2019-08-12 22:50:26 +0800

    小程序

---
+++ pages/content/content.js
@@ -0,0 +1,77 @@
+// pages/content/content.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    imgURLs: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565513954354&di=8a502de7a064a11f007642b98914fc1f&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201801%2F26%2F20180126005333_T835x.jpeg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565513954354&di=63c459423a0a2ad9528f39992e88096f&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201809%2F22%2F20180922230455_VZhYu.thumb.700_0.jpeg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565513954349&di=787d5ecdb51a7403ae02ffc88f5b5729&imgtype=0&src=http%3A%2F%2Fi2.hdslb.com%2Fbfs%2Farchive%2F52878497d0a2eca89d147d35e462183cbfcc7c3a.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565513954348&di=7fc94e14023f056757033f82b6d54bd5&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201807%2F19%2F20180719125441_obrrc.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565514358183&di=285e5bcf9290109484847da4049b502b&imgtype=0&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201508%2F03%2F20150803143919_54YW2.jpeg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565514561889&di=aa23c8c179739908b70b195926a81910&imgtype=0&src=http%3A%2F%2Fi2.hdslb.com%2Fbfs%2Farchive%2F683d9f5f497ca89e4f2b12dc86207bc62ff24f9b.jpg'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
